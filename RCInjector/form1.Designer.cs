0c837384bc83c3c9f58c1fb69462b30d5b815364
Shadow <133883690+XarTeSV2@users.noreply.github.com>
2023-07-26 16:33:28 +0300

    Add files via upload

---
+++ RCInjector/form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace RCInjector
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Dlls = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(146, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(146, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = " Clear List";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(158, 182);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 42);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Inject";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioButton1.Location = new System.Drawing.Point(15, 184);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Manual";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioButton2.Location = new System.Drawing.Point(15, 207);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(72, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Automatic";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Lütfen Dll seçiniz.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.checkBox1.Location = new System.Drawing.Point(15, 161);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(105, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Close after Inject";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Dlls
+            // 
+            this.Dlls.FormattingEnabled = true;
+            this.Dlls.Location = new System.Drawing.Point(12, 60);
+            this.Dlls.Name = "Dlls";
+            this.Dlls.Size = new System.Drawing.Size(120, 95);
+            this.Dlls.TabIndex = 12;
+            this.Dlls.SelectedIndexChanged += new System.EventHandler(this.DLLs_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(146, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(245, 241);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Dlls);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.ForeColor = System.Drawing.Color.BlueViolet;
+            this.Name = "Form1";
+            this.Text = "RCInjector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox Dlls;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RCInjector/form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using System.Data.Common;
+using System.Net;
+using System.Runtime.Remoting;
+using System.Security.Policy;
+using System.Xml.Linq;
+
+namespace RCInjector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Private TargetProcessHandle As Integer
+    Private pfnStartAddr As Integer
+    Private pszLibFileRemote As String
+    Private TargetBufferSize As Integer
+
+    Public Const PROCESS_VM_READ = &H10
+    Public Const TH32CS_SNAPPROCESS = &H2
+    Public Const MEM_COMMIT = 4096
+    Public Const PAGE_READWRITE = 4
+    Public Const PROCESS_CREATE_THREAD = (&H2)
+    Public Const PROCESS_VM_OPERATION = (&H8)
+    Public Const PROCESS_VM_WRITE = (&H20)
+    Dim DLLFileName As String
+    Public Declare Function ReadProcessMemory Lib "kernel32"(_
+    ByVal hProcess As Integer, _
+    ByVal lpBaseAddress As Integer, _
+    ByVal lpBuffer As String, _
+    ByVal nSize As Integer, _
+    ByRef lpNumberOfBytesWritten As Integer) As Integer
+
+    Public Declare Function LoadLibrary Lib "kernel32" Alias "LoadLibraryA"(_
+    ByVal lpLibFileName As String) As Integer
+
+    Public Declare Function VirtualAllocEx Lib "kernel32"(_
+    ByVal hProcess As Integer, _
+    ByVal lpAddress As Integer, _
+    ByVal dwSize As Integer, _
+    ByVal flAllocationType As Integer, _
+    ByVal flProtect As Integer) As Integer
+
+    Public Declare Function WriteProcessMemory Lib "kernel32"(_
+    ByVal hProcess As Integer, _
+    ByVal lpBaseAddress As Integer, _
+    ByVal lpBuffer As String, _
+    ByVal nSize As Integer, _
+    ByRef lpNumberOfBytesWritten As Integer) As Integer
+
+    Public Declare Function GetProcAddress Lib "kernel32"(_
+    ByVal hModule As Integer, ByVal lpProcName As String) As Integer
+
+    Private Declare Function GetModuleHandle Lib "Kernel32" Alias "GetModuleHandleA"(_
+    ByVal lpModuleName As String) As Integer
+
+    Public Declare Function CreateRemoteThread Lib "kernel32"(_
+    ByVal hProcess As Integer, _
+    ByVal lpThreadAttributes As Integer, _
+    ByVal dwStackSize As Integer, _
+    ByVal lpStartAddress As Integer, _
+    ByVal lpParameter As Integer, _
+    ByVal dwCreationFlags As Integer, _
+    ByRef lpThreadId As Integer) As Integer
+
+    Public Declare Function OpenProcess Lib "kernel32"(_
+    ByVal dwDesiredAccess As Integer, _
+    ByVal bInheritHandle As Integer, _
+    ByVal dwProcessId As Integer) As Integer
+
+    Private Declare Function FindWindow Lib "user32" Alias "FindWindowA"(_
+    ByVal lpClassName As String, _
+    ByVal lpWindowName As String) As Integer
+
+    Private Declare Function CloseHandle Lib "kernel32" Alias "CloseHandleA"(_
+    ByVal hObject As Integer) As Integer
+    Dim ExeName As String = IO.Path.GetFileNameWithoutExtension(Application.ExecutablePath)
+    Private Sub Inject()
+        On Error GoTo 1 ' If error occurs, app will close without any error messages 
+        Timer1.Stop()
+        Dim TargetProcess As Process() = Process.GetProcessesByName(TextBox1.Text)
+        TargetProcessHandle = OpenProcess(PROCESS_CREATE_THREAD Or PROCESS_VM_OPERATION Or PROCESS_VM_WRITE, False, TargetProcess(0).Id)
+        pszLibFileRemote = OpenFileDialog1.FileName
+        pfnStartAddr = GetProcAddress(GetModuleHandle("Kernel32"), "LoadLibraryA")
+        TargetBufferSize = 1 + Len(pszLibFileRemote)
+        Dim Rtn As Integer
+        Dim LoadLibParamAdr As Integer
+        LoadLibParamAdr = VirtualAllocEx(TargetProcessHandle, 0, TargetBufferSize, MEM_COMMIT, PAGE_READWRITE)
+        Rtn = WriteProcessMemory(TargetProcessHandle, LoadLibParamAdr, pszLibFileRemote, TargetBufferSize, 0)
+        CreateRemoteThread(TargetProcessHandle, 0, 0, pfnStartAddr, LoadLibParamAdr, 0, 0)
+        CloseHandle(TargetProcessHandle)
+1:      Me.Show()
+    End Sub
+            InitializeComponent();
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            For i As Integer = (Dlls.SelectedItems.Count - 1) To 0 Step - 1
+            Dlls.Items.Remove(Dlls.SelectedItems(i))
+        Next
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer1.Interval = 2
+        Timer1.Start()
+        RadioButton1.Checked = True
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Timer1.Start()
+        CheckBox1.Checked = True
+        CheckBox1.Enabled = False
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            If IO.File.Exists(OpenFileDialog1.FileName) Then
+
+       End If
+       Dim TargetProcess As Process() = Process.GetProcessesByName(TextBox1.Text)
+        If TargetProcess.Length = 0 Then
+
+            Me.Label1.Text = ("İnject İçin Bekleniyor " + TextBox1.Text + ".exe" + "....")
+        Else
+            Timer1.Stop()
+            Me.Label1.ForeColor = Color.Green
+            Me.Label1.Text = "İnject Tamamlandı!"
+            Call Inject()
+            If CheckBox1.Checked = True Then
+                Me.Close()
+            Else
+            End If
+        End If
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Dlls.Items.Clear()
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DLLs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Timer1.Stop()
+        CheckBox1.Checked = True
+        CheckBox1.Enabled = True
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog1.Filter = "DLL (*.dll) |*.dll"
+        OpenFileDialog1.ShowDialog()
+        Dim FileName As String
+        FileName = OpenFileDialog1.FileName.Substring(OpenFileDialog1.FileName.LastIndexOf("\"))
+        Dim DllFileName As String = FileName.Replace("\", "")
+        Me.Dlls.Items.Add(DllFileName)
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            If IO.File.Exists(OpenFileDialog1.FileName) Then
+         Dim TargetProcess As Process() = Process.GetProcessesByName(TextBox1.Text)
+            If TargetProcess.Length = 0 Then
+                Me.Label1.ForeColor = Color.Red
+                Me.Label1.Text = ("İnject İçin Bekleniyor " + TextBox1.Text + ".exe" + "....")
+            Else
+                Timer1.Stop()
+                Me.Label1.ForeColor = Color.Green
+                Me.Label1.Text = "İnject Tamamlandı!"
+                Call Inject()
+                If CheckBox1.Checked = True Then
+                    Me.Close()
+                End If
+
+
+            End If
+        End If
+        }
+    }
+}
+
+
